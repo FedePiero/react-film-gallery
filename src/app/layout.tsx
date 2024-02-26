25b0d11c54a4649e733f45ecbcaa13b231cac7cc
Federico <federicoluca99@gmail.com>
2024-02-26 23:30:52 +0100

    fix: add header correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,14 +11,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
-}>,) {
+}>) {
   return (
     <html lang="en">
-      <Header></Header>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header></Header>
+        {children}
+      </body>
     </html>
   );
 }
