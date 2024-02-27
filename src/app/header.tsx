d859bea8d0ff85b2fbadd0342cc56dcc77455ec9
Federico <federicoluca99@gmail.com>
2024-02-27 23:13:03 +0100

    feat: add search for a film

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,9 +6,9 @@ import Button from "@mui/material/Button";
 export default function header() {
   return (
     <div className="bg-red-900 min-h-12 flex justify-center">
-      <Button color="secondary" size="large" variant="text"  style={{color: "white"}}>
-        Home
-      </Button>
+      <h1 className="m-2 text-white">
+        FILM FOR EVERYONE
+      </h1>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,40 @@
+"use client";
+import { Button, TextField } from "@mui/material";
 import Image from "next/image";
+import React, { useState } from "react";
+
+const films = [
+  { photo: "troy.jpeg", title: "Troy" },
+  {
+    photo: "unceupponatimeinhollywood.jpeg",
+    title: "Once Uppon A Time In Hollywood",
+  },
+  { photo: "oppenheimer.jpeg", title: "Oppenheimer" },
+  { photo: "napoleon.jpeg", title: "Napoleon" },
+];
 
 export default function Home() {
-  let films = [
-    { photo: "troy.jpeg", title: "Troy" },
-    {
-      photo: "unceupponatimeinhollywood.jpeg",
-      title: "Once Uppon A Time In Hollywood",
-    },
-    { photo: "oppenheimer.jpeg", title: "Oppenheimer" },
-    { photo: "napoleon.jpeg", title: "Napoleon" },
-  ];
+  let [filmFiltered, setFilmFiltered] = useState(films);
+
+  function handleClick(value) {
+    const newArray = JSON.parse(
+      JSON.stringify(films.filter((o) => o.title.includes(value)))
+    );
+    setFilmFiltered(newArray);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-24">
-      <div className="max-w-6xl w-full items-center justify-between lg:flex gap-3 flex-wrap">
-        {films.map((film, index) => {
+      <TextField
+        label="Search"
+        variant="outlined"
+        className="m-0"
+        onChange={(e) => {
+          handleClick(e.target.value);
+        }}
+      />
+      <div className="max-w-6xl w-full items-center justify-between lg:flex gap-3 pt-0 flex-wrap">
+        {filmFiltered.map((film, index) => {
           return (
             <div className="max-w-1xl max-h-96" key={index}>
               <Image
