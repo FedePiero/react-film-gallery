a3201a8be11be5418a52b0cd0302780f06ff1840
Federico <federicoluca99@gmail.com>
2024-02-27 22:17:22 +0100

    feat: add material ui feat: add header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,11 +1,14 @@
 // Filename - Header.js
 
-import { Button } from "@material-tailwind/react";
+import * as React from "react";
+import Button from "@mui/material/Button";
 
 export default function header() {
   return (
     <div className="bg-red-900 min-h-12 flex justify-center">
-      <Button variant="filled">filled</Button>
+      <Button color="secondary" size="large" variant="text"  style={{color: "white"}}>
+        Home
+      </Button>
     </div>
   );
 }
